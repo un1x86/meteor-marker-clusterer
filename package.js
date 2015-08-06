52748f3e371e22c437ac109d972ceb5be0b964dc
Rob Lingle <rob@oak.works>
2015-08-06 10:38:35 -0400

    removed submodule and copied files in

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'oakworks:marker-clusterer',
-  version: '0.0.1',
+  version: '0.1.0',
   summary: 'A marker clustering library for the Google Maps JavaScript API v3, packaged for Meteor',
   git: 'https://github.com/oakworks/meteor-marker-clusterer',
   documentation: 'README.md'
@@ -8,5 +8,5 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('lib/js-marker-clusterer/src/markerclusterer_compiled.js', 'client');
+  api.addFiles('markerclusterer.js', 'client');
 });
